fff0f96e4103003f1936c894a6bd5574b1b86a99
Mohammed-Atia22 <153985518+Mohammed-Atia22@users.noreply.github.com>
2026-03-24 07:56:50 +0200

    Revert "Adding supplier code"

--- utils/Error handlers/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-import BaseError from "./BaseError.js";
-
-export class NotFoundError extends BaseError {
-  constructor(message = "Resource Not Found") {
-    super(message, 404);
-    this.errors = [{ msg: message }];
-  }
-}
